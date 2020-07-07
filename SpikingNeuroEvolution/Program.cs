3b5820c26c2a1e3116540cb7237943764b54dbe2
Sergey Gerasimov <gerich.home@gmail.com>
2020-07-07 10:10:31 +0300

    genetics

--- SpikingNeuroEvolution/Program.cs
+++ SpikingNeuroEvolution/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 using System.Threading;
 using static System.Math;
@@ -67,7 +68,12 @@ namespace SpikingNeuroEvolution
         {
             var gInf = 1 / (1 + (_target.U < -50 ? 0 : Exp(a * _target.U) * Mg / b));
 
-            return gInf * gNMDA * N * _source.Spikes.Sum(ts => Exp((ts - t) / tDecay) - Exp((ts - t) / tRise));
+            return gInf * gNMDA * N * _source.Spikes.Sum(ts => SpikeTrace(t, ts));
+        }
+
+        private double SpikeTrace(double t, double ts)
+        {
+            return Exp((ts - t) / tDecay) - Exp((ts - t) / tRise);
         }
 
         public double SynapseInput(double t)
@@ -86,25 +92,43 @@ namespace SpikingNeuroEvolution
 
     class NeuralNet
     {
-        public readonly IReadOnlyList<Neuron> Neurons;
+        public IReadOnlyList<Neuron> Neurons;
         
         private double t;
 
         public NeuralNet()
+        {
+        }
+
+        public NeuralNet(IEnumerable<Neuron> neurons)
+        {
+            Neurons = neurons.ToList();
+        }
+
+        public void Initialize()
         {
             var n1 = new Neuron();
             var n2 = new Neuron();
             var n3 = new Neuron();
             var n4 = new Neuron();
             var n5 = new Neuron();
-            Neurons = new List<Neuron> {n1, n2, n3, n4, n5};
+            var n6 = new Neuron();
+            var n7 = new Neuron();
+            var n8 = new Neuron();
+            var n9 = new Neuron();
+            Neurons = new List<Neuron> {n1, n3, n4, n5, n6, n7, n8, n9, n2};
 
-            Connect(n1, n2, 0.01);
-            Connect(n1, n3, 0.01);
-            Connect(n2, n5, 0.1);
-            Connect(n1, n4, 0.01);
-            Connect(n3, n4, 0.01);
-            Connect(n4, n5, 0.01);
+            var d = 0.07;
+            Connect(n1, n2, 1 * 0.01);
+            Connect(n3, n4, 1 * 0.01); // 0.045 -> 0.01
+            Connect(n4, n5, 1 * d);
+            Connect(n5, n6, 1 * d);
+            Connect(n6, n7, 1 * d);
+            Connect(n7, n8, 1 * d);
+            Connect(n8, n9, 1 * d);
+            //Connect(n1, n4, 0.01);
+            //Connect(n3, n4, 0.01);
+            //Connect(n4, n5, 0.01);
         }
 
         public void Connect(Neuron source, Neuron target, double w)
@@ -116,7 +140,7 @@ namespace SpikingNeuroEvolution
         public void Update(double dt, IReadOnlyDictionary<Neuron, double> additionalCurrent)
         {
             var inputs = Neurons.ToDictionary(neuron => neuron,
-                neuron => neuron.InputCurrent(t, additionalCurrent[neuron]));
+                neuron => neuron.InputCurrent(t, additionalCurrent.GetValueOrDefault(neuron, 0)));
 
             foreach (var neuron in Neurons)
             {
@@ -136,12 +160,14 @@ namespace SpikingNeuroEvolution
                 F(neuron.InputCurrent(t, additionalCurrent[neuron]));
             }
 
+            Console.WriteLine();
             Console.WriteLine("U+65");
             foreach (var neuron in Neurons)
             {
                 F(neuron.U + 65);
             }
 
+            Console.WriteLine();
             Console.WriteLine("Spikes");
             foreach (var neuron in Neurons)
             {
@@ -157,18 +183,234 @@ namespace SpikingNeuroEvolution
         }
     }
 
+    class Chromosome
+    {
+        public readonly ImmutableHashSet<EdgeGene> EdgeGenes;
+        public readonly int Nodes;
+
+        public Chromosome(IEnumerable<EdgeGene> edgeGenes, int nodeGenes)
+            : this(edgeGenes.ToImmutableHashSet(), nodeGenes)
+        {
+        }
+
+        public Chromosome(ImmutableHashSet<EdgeGene> edgeGenes, int nodes)
+        {
+            EdgeGenes = edgeGenes;
+            Nodes = nodes;
+        }
+
+        public Chromosome MutateAddNode(Func<ImmutableHashSet<EdgeGene>, EdgeGene> chooseGene)
+        {
+            var gene = chooseGene(EdgeGenes);
+            var disabledGene = gene.Disable();
+            var g1 = new EdgeGene(new GeneType(disabledGene.GeneType.From, Nodes), disabledGene.Weight, true);
+            var g2 = new EdgeGene(new GeneType(Nodes, disabledGene.GeneType.To), disabledGene.Weight, true);
+
+            var newEdgeGenes = EdgeGenes.Remove(gene).Union(new[]
+            {
+                disabledGene,
+                g1,
+                g2
+            });
+
+            return new Chromosome(newEdgeGenes, Nodes + 1);
+        }
+
+        public Chromosome MutateChangeWeight(Func<ImmutableHashSet<EdgeGene>, (EdgeGene, double)> chooseGeneAndWeight)
+        {
+            var (gene, newWeight) = chooseGeneAndWeight(EdgeGenes);
+
+            var newEdgeGenes = EdgeGenes
+                .Remove(gene)
+                .Add(gene.ChangeWeight(newWeight));
+
+            return new Chromosome(newEdgeGenes, Nodes);
+        }
+
+        public Chromosome MutateChangeEnabled(Func<ImmutableHashSet<EdgeGene>, (EdgeGene, bool)> chooseGeneAndEnabled)
+        {
+            var (gene, newIsEnabled) = chooseGeneAndEnabled(EdgeGenes);
+
+            var newEdgeGenes = EdgeGenes
+                .Remove(gene)
+                .Add(gene.ChangeEnabled(newIsEnabled));
+
+            return new Chromosome(newEdgeGenes, Nodes);
+        }
+
+        public static Chromosome Crossover(Chromosome chromosomeA, Chromosome chromosomeB,
+            Func<EdgeGene, EdgeGene, EdgeGene> crossoverMatchingGene)
+        {
+            var edgeGenesA = EdgeGenes(chromosomeA);
+            var edgeGenesB = EdgeGenes(chromosomeB);
+
+            var keysA = edgeGenesA.Keys.ToImmutableHashSet();
+            var keysB = edgeGenesB.Keys.ToImmutableHashSet();
+
+            var disjointGenesA = keysA.Except(keysB).Select(key => edgeGenesA[key]);
+            var disjointGenesB = keysB.Except(keysA).Select(key => edgeGenesB[key]);
+            var crossedGenes = keysA.Intersect(keysB)
+                .Select(key => crossoverMatchingGene(edgeGenesA[key], edgeGenesB[key]));
+
+            return new Chromosome(disjointGenesA
+                    .Concat(disjointGenesB)
+                    .Concat(crossedGenes),
+                Max(chromosomeA.Nodes, chromosomeB.Nodes));
+
+            ImmutableDictionary<GeneType, EdgeGene> EdgeGenes(Chromosome chromosome)
+            {
+                return chromosome.EdgeGenes.ToImmutableDictionary(gene => gene.GeneType, gene => gene);
+            }
+        }
+    }
+
+    class EvaluatedChromosome
+    {
+        public Chromosome Chromosome { get; }
+        public double Fitness { get; }
+
+        public EvaluatedChromosome(Chromosome chromosome, double fitness)
+        {
+            Chromosome = chromosome;
+            Fitness = fitness;
+        }
+    }
+
+    struct GeneType
+    {
+        public readonly int From;
+        public readonly int To;
+
+        public GeneType(int @from, int to)
+        {
+            From = @from;
+            To = to;
+        }
+    }
+
+    class EdgeGene
+    {
+        public readonly double Weight;
+        public readonly bool IsEnabled;
+        public readonly GeneType GeneType;
+
+        public EdgeGene(GeneType geneType, double weight, bool isEnabled)
+        {
+            GeneType = geneType;
+            Weight = weight;
+            IsEnabled = isEnabled;
+        }
+
+        public EdgeGene Disable()
+        {
+            return ChangeEnabled(false);
+        }
+
+        public EdgeGene Enable()
+        {
+            return ChangeEnabled(true);
+        }
+
+        public EdgeGene ChangeEnabled(bool newIsEnabled)
+        {
+            return IsEnabled == newIsEnabled
+                ? this
+                : new EdgeGene(GeneType, Weight, newIsEnabled);
+        }
+
+        public EdgeGene ChangeWeight(double newWeight)
+        {
+            return Weight != newWeight
+                ? new EdgeGene(GeneType, newWeight, IsEnabled)
+                : this;
+        }
+    }
+
+    class CPNN
+    {
+        private readonly Chromosome _chromosome;
+
+        public CPNN(Chromosome chromosome)
+        {
+            _chromosome = chromosome;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            //TestNeat();
+            TestSNN();
+        }
+
+        private static void TestNeat()
+        {
+            var chromosomes = Enumerable.Range(0, 100)
+                .Select(p => new Chromosome(new[]
+                    {
+                        new EdgeGene(new GeneType(0, 2), 1, true),
+                        new EdgeGene(new GeneType(1, 2), 1, true)
+                    }, 3)
+                )
+                .ToList();
+
+            var list = chromosomes.Select(chromosome => Evaluate(chromosome, new[] {10.0, 20.0}, 1, 5, 100))
+                .ToList();
+
+            foreach (var d in list)
+            {
+                Console.WriteLine(d.Sum());
+            }
+        }
+
+
+        public static IReadOnlyList<int> Evaluate(Chromosome chromosome, IReadOnlyList<double> input, int outSize, double time, int iterations)
+        {
+            var neuralNetwork = BuildNeuralNetwork(chromosome);
+
+            neuralNetwork
+                .Update(iterations, input.Select((d, i) => (d, i))
+                    .ToDictionary(t => neuralNetwork.Neurons[t.i], t => t.d));
+
+            return neuralNetwork.Neurons
+                .Skip(input.Count)
+                .Take(outSize)
+                .Select(neuron => neuron.Spikes.Count)
+                .ToList();
+        }
+
+        private static NeuralNet BuildNeuralNetwork(Chromosome chromosome)
+        {
+            var neurons = chromosome.EdgeGenes
+                .SelectMany(gene => new[] { gene.GeneType.From, gene.GeneType.To })
+                .Distinct()
+                .OrderBy(nodeIndex => nodeIndex)
+                .Select(nodeIndex => new Neuron())
+                .ToList();
+
+            var neuralNetwork = new NeuralNet(neurons);
+
+            foreach (var gene in chromosome.EdgeGenes.Where(gene => gene.IsEnabled))
+            {
+                neuralNetwork.Connect(neurons[gene.GeneType.From], neurons[gene.GeneType.To], gene.Weight);
+            }
+
+            return neuralNetwork;
+        }
+
+        private static void TestSNN()
+        {
             var nn = new NeuralNet();
+            nn.Initialize();
 
             var additionalCurrent = nn.Neurons.ToDictionary(neuron => neuron, neuron => 0.0);
-            additionalCurrent[nn.Neurons[0]] = 20;
+            additionalCurrent[nn.Neurons[0]] = 19;
+            additionalCurrent[nn.Neurons[1]] = 19;
 
             while (true)
             {
-                for (int i = 0; i < 100; i++)
+                for (int i = 0; i < 1000; i++)
                 {
                     nn.Update(0.01, additionalCurrent);
                 }
